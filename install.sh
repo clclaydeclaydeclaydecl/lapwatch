064d49717c71efa1b62d191da2ce63e15b9efb8a
clayde loyola <laxusss000@gmail.com>
2026-06-28 15:50:11 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo curl -sS https://raw.githubusercontent.com/clclaydeclaydeclaydecl/lapwatch/main/lapwatch -o /usr/local/bin/lapwatch
+sudo chmod +x /usr/local/bin/lapwatch
+echo "Lapwatch installed. Run 'lapwatch' to start."
